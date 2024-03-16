fb6fcd6f30b2671d8465d1c05ab23ec2dd393bb8
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-03-16 21:04:15 +0500

    all id taminlando

---
+++ R-1-9/index.js
@@ -0,0 +1,11 @@
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
+var button5 = document.getElementById("button5");
+var button6 = document.getElementById("button6");
+var button7 = document.getElementById("button7");
+var button8 = document.getElementById("button8");
+
+var asosiyDiv = document.getElementById("asos");
+var speedDiv = document.getElementById("speed");
