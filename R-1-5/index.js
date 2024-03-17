03cd79b3d2d0a49f0766c977dee036837146429d
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-03-17 19:33:14 +0500

    no commit

--- R-1-5/index.js
+++ R-1-5/index.js
@@ -1,4 +1,25 @@
-document.getElementById("toggleButton").addEventListener("click", function () {
-  var animateDiv = document.getElementById("animateDiv");
-  animateDiv.classList.toggle("animate");
-});
+// function replaceVar() {
+//   let firstInput = document.getElementById("birinchi");
+//   let secondInput = document.getElementById("ikkinchi");
+//   let New_data = document.querySelector(".new_data");
+
+//   const message = `Sizning to'liq ismingiz: ${firstInput.value}, ${secondInput.value}`;
+//   New_data.innerText = message;
+//   console.log(message);
+// }
+
+let firstInput = document.getElementById("birinchi");
+let secondInput = document.getElementById("ikkinchi");
+let new_data = document.querySelector(".new_data");
+
+function replaceVar() {
+  const message = `Sizning to'liq ismingiz: ${secondInput.value}, ${firstInput.value} <button onclick="correct()">Change</button>`;
+  new_data.innerHTML = message;
+  console.log(message);
+}
+
+function correct() {
+  const message = `Sizning to'liq ismingiz: ${firstInput.value}, ${secondInput.value} ga o'zgartirildi`;
+  new_data.innerText = message;
+  console.log(message);
+}
